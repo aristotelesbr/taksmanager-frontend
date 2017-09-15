dc76e3dd7f4a46e3e9ffade7c03928e176089956
Aristoteles <aristotelesbr2014@gmail.com>
2017-09-14 22:18:10 -0300

    Adding navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+// 1- Importa o componente
+import { Component } from '@angular/core';
+
+// 2 -  define meta dados
+@Component({
+	selector: 'navbar',
+	templateUrl: './navbar.component.html'
+})
+
+// 3 - Exporta
+export class NavbarComponent{
+
+}
